0cedddda47ed43ca855138d2150484a201126b7f
Jackie <13265466114@163.com>
2019-10-12 14:15:08 +0800

    add suggestion

--- test.py
+++ test.py
@@ -15,7 +15,18 @@ gender = 1 if gender == "男" else 0
 BMI = weight / (height * height)
 body_fat = (1.2 * BMI + 0.23 * age - 0.54 - 10.8 * gender) / 100
 
-print(name, age, height, weight, gender)
+
+body_fat_rate = ((0.25, 0.28), (0.15, 0.18))
+min_fat, max_fat = body_fat_rate[gender]
+
+result = "正常"
+if body_fat < min_fat:
+    result = "偏瘦"
+elif body_fat > max_fat:
+    result = "偏胖"
+
+
+# print(name, age, height, weight, gender)
 
 print("BMI = %f" % BMI, "体脂率 = %f" % body_fat)
-
+print("您的体重属于%s" % result)
