09dc4873f6504aab4bf54ffd9c8ac71fe71c7bb0
Jackie <13265466114@163.com>
2019-10-15 18:40:46 +0800

    file traverse for second dir

--- FileSearch.py
+++ FileSearch.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path = "/Users/jackie/Downloads"
+path = "/Users/jackie/Downloads/Python阶段资料"
 
 if not os.path.exists(path):
     exit()
@@ -10,6 +10,23 @@ os.chdir(path)
 file_list = os.listdir("./")
 # print(file_list)
 
-for file_name in  file_list:
-    print(file_name)
+print(os.getcwd())
 
+# for file_name in file_list:
+    # print("-" + file_name)
+    # if os.path.isdir(file_name):
+    #     for sub_files in os.listdir(file_name):
+    #         print("--" + sub_files)
+
+
+def file_traverse(file_name, index = 0):
+    index += 1
+    if os.path.isdir(file_name):
+        for sub_file in os.listdir(file_name):
+
+            print(index * "-" + sub_file)
+            file_traverse(sub_file, index)
+    else:
+        return
+
+file_traverse(path)
