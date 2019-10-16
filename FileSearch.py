7dcf1b0dcaeef030a8440b695b077566655dc5be
Jackie <13265466114@163.com>
2019-10-16 19:51:26 +0800

    final files traverse

--- FileSearch.py
+++ FileSearch.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path = "/Users/jackie/Downloads/Python阶段资料/04-异常处理"
+path = "/Users/jackie/Downloads/Python阶段资料"
 
 if not os.path.exists(path):
     exit()
@@ -21,11 +21,12 @@ print(os.getcwd())
 
 def file_traverse(file_name, index=0):
     index += 1
+    # print("----------> current: " + file_name)
     if os.path.isdir(file_name):
         for sub_file in os.listdir(file_name):
-
+            sub_path = file_name + "/" + sub_file
             print(index * "-" + sub_file)
-            file_traverse(sub_file, index)
+            file_traverse(sub_path, index)
     else:
         return
 
