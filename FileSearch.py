5c13d812de6f304fcb40a903b9ba94f9d6a67b01
Jackie <13265466114@163.com>
2019-12-11 16:06:05 +0800

    file rename

--- FileSearch.py
+++ FileSearch.py
@@ -16,12 +16,17 @@ if not os.path.exists(path):
 
 def file_rename(old_name, new_name, index=0):
     if os.path.exists(old_name):
-        suffix = old_name.split('.')[-1]
-
-        print(old_name)
-        name = "%s %s.%s" % (new_name, index, suffix)
-        print(name)
-        os.rename(old_name, name)
+        if "." in old_name:
+            suffix = old_name.split('.')[-1]
+            if 0 == index:
+                print(suffix)
+                current_name = "%s %s.%s" % (new_name, index, suffix)
+                print(current_name)
+            else:
+                current_name = "%s.%s" % (new_name, suffix)
+            os.rename(old_name, current_name)
+        else:
+            os.rename(old_name, new_name)
 
 
 def file_traverse(file_name, index=0):
@@ -29,9 +34,7 @@ def file_traverse(file_name, index=0):
 
     if os.path.isdir(file_name):
         for sub_file in os.listdir(file_name):
-            # print(index * "-" + sub_file)
-            num = filter(str.isdigit(), sub_file)
-            print(num)
+            # print(index * "-" + sub_file))
 
             # 重命名
             # index += 1
@@ -40,7 +43,7 @@ def file_traverse(file_name, index=0):
             sub_path = file_name + "/" + sub_file
             file_traverse(sub_path, index)
     else:
-        # print(file_name)
+        print(file_name)
         return
 
 
