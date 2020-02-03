160eba9adfaf84ffe0e0b2e85b5c3b10618ddfe3
Jackie <13265466114@163.com>
2020-02-03 11:35:15 +0800

    1. update requirements.txt

--- PandasExcel.py
+++ PandasExcel.py
@@ -5,12 +5,15 @@ import pandas as pd
 # print(df)
 
 
-df = pd.read_excel("./demo.xlsx")
+# df = pd.read_excel("./demo.xlsx")
 # df.columns = ["a1", "a2", "a3"]
 
-print(df.columns)
-print(df.head(2))
-print(df.tail(2))
+# print(df.columns)
+# print(df.head(2))
+# print(df.tail(2))
+
+s1 = pd.Series(data=["100", "200", "300"], index=["x", "y", "z"])
+print(s1)
 
 # df.to_excel("demo.xlsx")
 # print("success")
