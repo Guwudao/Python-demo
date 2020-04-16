e729a00fbe700045631ac3947e81b19dfe56c6f4
Jackie <13265466114@163.com>
2020-04-17 00:57:02 +0800

    1. add file move method

--- FileSearch.py
+++ FileSearch.py
@@ -1,5 +1,5 @@
 import os
-# import shutil
+import shutil
 
 
 # path = "/Users/jj/Desktop/book/"
@@ -33,9 +33,15 @@ def file_rename(old_name, new_name, index=0):
 
 
 def file_name_append(path, old_name, prefix):
-    suffix = path.split('.')[-1]
+    if not os.path.isdir(path):
+        print(path)
+        new_name = prefix + " - " + old_name
+        index = path.rfind("/") + 1
+        final = path[0:index] + new_name
+        print(final)
+        print("-" * 30)
 
-    print(prefix + " - " + old_name + suffix)
+        os.rename(path, final)
 
 
 def file_filter(path, file_name):
@@ -46,13 +52,23 @@ def file_filter(path, file_name):
         os.remove(full_path)
 
 
+def file_move(path, condition):
+    default_dir = "/Users/jackie/Downloads/周杰伦zip/"
+    if not os.path.exists(default_dir):
+        os.makedirs(default_dir)
+
+    if path.split(".")[-1] == "zip":
+        shutil.move(path, default_dir)
+
+
 dir_name = ""
 def file_traverse(file_name, index=0):
     index += 1
     if os.path.isdir(file_name):
-        if not file_name.split("/")[-1] == "MP3":
-            dir_name = file_name.split("/")[-1]
-            print(dir_name)
+        # if not file_name.split("/")[-1] == "MP3":
+        #     global dir_name
+        #     dir_name = file_name.split("/")[-1]
+        #     print(dir_name)
 
         for sub_file in os.listdir(file_name):
 
@@ -70,16 +86,13 @@ def file_traverse(file_name, index=0):
             # index += 1
             # file_rename(sub_path, "MSS", index)
 
-            file_name_append(sub_path, sub_file, dir_name)
+            # 名称拼接
+            # file_name_append(sub_path, sub_file, dir_name)
+
+            # 移动文件夹
+            # file_move(sub_path, "zip")
 
             file_traverse(sub_path, index)
-    else:
-        suffix = file_name.split('.')[-1]
-        # if suffix == "xltd":
-        #     print(file_name)
-        #     os.remove(file_name)
-        return
-
 
 
 file_traverse(path)
