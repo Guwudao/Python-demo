c205247917f59cd1410bfabcd0bc4d8553c28abd
Jackie <13265466114@163.com>
2020-04-19 00:58:25 +0800

    1. some updates

--- Character-slicing.py
+++ Character-slicing.py
@@ -47,31 +47,31 @@ new = """1. WeChat 许月英 【11人】
 
 today = """
 #接龙
-0401健康打卡
+4.17健康打卡
 
-1. WeChat 许月英【11人】
-2. Mvtm 张璐 4人
-3. Staff App & VB 苟肖朋 【13】
-4. Apollo8 陈少轩 10人
-5. MobileX & jade 林俊杰 【7人】
-6. GBA QR 朱纯佳 10人
-7. Payment 王勇 14人
-8. Mobile 1.5 胡超 6人
-9. CCA 赵得华 10人
-10. MobileR & RBBLending 候梦璇 〔10人〕
-11. apollo7 刘昌旭 9人
-12. SaaS 赖淑茹 6人
-13. API 杨佳 【27人】
-14. China Mobile +Mobile Foudation 周亚 【12】
-15. GBAOMP 谢研 11人
-16. mobilex 李凯 14
-17. SMP 田晶/王杨 32人
+1. Mvtm 张璐 4人
+2. CCA 赵得华 10人
+3. China Mobile +Mobile Foudation 周亚 【12】
+4. GBA QR 朱纯佳 10人
+5. Staff APP & VB 苟肖朋 【12】
+6. MobileX & jade 林俊杰 【7人】
+7. Payment 王勇 15人
+8. SaaS 赖淑茹 6人
+9. 李凯 14人
+10. SMP 田晶/王杨 32人
+11. WeChat 许月英【11人】
+12. apollo7 刘昌旭 9人
+13. GBAOMP 谢研 11人
+14. Mobile 1.5 胡超 6人
+15. Apollo8 陈少轩 【10人】
 """
 
 
 leader_list = ['许月英', '苟肖朋', '王勇', '周亚', '朱纯佳', '张璐', '胡超', '赖淑茹', '林俊杰', '李凯', '陈少轩', '杨佳', '田晶/王杨', '刘昌旭', '赵得华', '谢研', '候梦璇', '王腾/林健卜']
 
 team_list = ['WeChat', 'Staff', 'Payment', 'China', 'GBA', 'MVTM', 'Mobile', 'SaaS', 'MobileX', 'Mobilex', 'Apollo', 'API', 'SMP', 'louis', 'cca', 'GBA', 'MobileR', 'GBA']
+
+count_list = [11, 12, 15, 12, 10, 4, 6, 6, 7, 14, 10, 27, 32, 9, 10, 11, 10, 11]
 
 l_list = []
 def get_leader_list(content) -> []:
@@ -87,17 +87,12 @@ def get_leader_list(content) -> []:
 
 
 def unclock_list(today):
-    result_list = []
-    for team, leader in zip(team_list, leader_list):
-        if leader not in today:
-            unclock = "未打卡：" + team + " " + leader
-            result_list.append(unclock)
-            # print(unclock)
 
     index = today.index("1.")
     new_today = today[index:]
     today_list = new_today.split("\n")
     sum = 0
+    num, current_count_list =[], []
 
     for t in today_list:
         t_list = t.split(".")
@@ -107,7 +102,18 @@ def unclock_list(today):
 
         if len(num) > 1:
             sum += int(num[1])
+            current_count_list.append(int(num[1]))
         sum += int(num[0])
+        current_count_list.append(int(num[0]))
+
+    print(current_count_list)
+
+    result_list = []
+    for team, leader, count in zip(team_list, leader_list, count_list):
+        if leader not in today:
+            unclock = "未打卡：" + team + " " + leader + " " + str(count) + "人"
+            result_list.append(unclock)
+            # print(unclock)
 
     print(sum)
     print(result_list)
